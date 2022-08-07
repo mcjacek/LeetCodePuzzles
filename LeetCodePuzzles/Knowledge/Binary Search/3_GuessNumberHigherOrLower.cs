3f2594af062aa085e4a8ce4e31bffdb5ab8fd9e9
Jacek Macura <jacek.macura@gmail.com>
2022-08-07 16:39:21 +0200

    Guess Number Higher Or Lower: URLs

--- LeetCodePuzzles/Knowledge/Binary Search/3_GuessNumberHigherOrLower.cs
+++ LeetCodePuzzles/Knowledge/Binary Search/3_GuessNumberHigherOrLower.cs
@@ -7,6 +7,8 @@ using System.Text;
 namespace LeetCodePuzzles.Knowledge.Binary_Search
 {
     /// <summary>
+    /// https://leetcode.com/explore/learn/card/binary-search/125/template-i/951/
+    /// https://leetcode.com/problems/guess-number-higher-or-lower/solution/
     /// We are playing the Guess Game. The game is as follows:
     ///    I pick a number from 1 to n.You have to guess which number I picked.
     ///    Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
